590216a0f63b2c205b2bb01455689c96a8717eae
Tayipztrk <tayipztrk@icloud.com>
2021-06-03 12:35:38 +0300

    fist version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import unsplash from './apis/unsplash'
+import SearchBar from './components/SearchBar'
+import ImageList from './components/ImageList'
 
-function App() {
+class App extends React.Component {
+
+  state= { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('/search/photos', {
+      params: {query: term}
+    });
+    this.setState({ images: response.data.results });
+  };
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container" style={{ marginTop: '10px'}}>
+      <SearchBar onSubmit={this.onSearchSubmit}/>
+      <ImageList images={this.state.images}/>
     </div>
   );
+}
 }
 
 export default App;
